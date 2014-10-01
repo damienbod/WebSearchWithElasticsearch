0538b1c78a5d69eb9600156c5695de86dc248042
damienbod <damien_bod@hotmail.com>
2014-10-01 08:32:18 +0200

    adding some css for the view

--- WebSearchWithElasticsearch/Controllers/SearchController.cs
+++ WebSearchWithElasticsearch/Controllers/SearchController.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Web.Mvc;
-using ElasticsearchCRUD;
 using WebSearchWithElasticsearch.Search;
 
 namespace WebSearchWithElasticsearch.Controllers
 {
 	public class SearchController : Controller
 	{
-		ISearchProvider _searchProvider = new ElasticSearchProvider();
-		public ActionResult Index()
+		readonly ISearchProvider _searchProvider = new ElasticSearchProvider();
+		public ActionResult Index(Skill model)
 		{
-			return View();
+			model = new Skill();
+			return View(model);
 		}
 
 		public ActionResult AddUpdate(Skill model)
 		{
-			model.Created = DateTime.UtcNow;
-			model.Updated = DateTime.UtcNow;
-			_searchProvider.AddUpdateEntity(model);
+			if (ModelState.IsValid)
+			{
+				model.Created = DateTime.UtcNow;
+				model.Updated = DateTime.UtcNow;
+				_searchProvider.AddUpdateEntity(model);
 
+				return View("Index", model);
+			}
 			return Redirect("Index");
+			
 		}
 
 		public JsonResult Search(string term)
 		{			
-			
 			return Json(_searchProvider.QueryString(term), "Skills", JsonRequestBehavior.AllowGet);
 		}
 	}
--- WebSearchWithElasticsearch/Search/Skill.cs
+++ WebSearchWithElasticsearch/Search/Skill.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebSearchWithElasticsearch.Search
 {
 	public class Skill
 	{
 		public long Id { get; set; }
+		[Required]
 		public string Name { get; set; }
+		[Required]
 		public string Description { get; set; }
 		public DateTimeOffset Created { get; set; }
 		public DateTimeOffset Updated { get; set; }
